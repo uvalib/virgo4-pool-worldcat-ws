55cad3ebe029cadc054b1349871089fc24379a6f
Jason Jordan <jlj5aj@virginia.edu>
2021-03-09 19:38:13 -0500

    pull in latest and greatest v4parser

--- cmd/worldcat.go
+++ cmd/worldcat.go
@@ -114,7 +114,7 @@ func (svc *ServiceContext) search(c *gin.Context) {
 	}
 
 	log.Printf("Raw query: %s, %+v %+v", req.Query, req.Pagination, req.Sort)
-	valid, errors := v4parser.ValidateWithTimeout(req.Query, 10)
+	valid, errors := v4parser.Validate(req.Query)
 	if valid == false {
 		log.Printf("ERROR: Query [%s] is not valid: %s", req.Query, errors)
 		c.String(http.StatusBadRequest, "Malformed search")
