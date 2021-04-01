f2ff84ffa8a4d0da3ac6c70b133aae525681f408
Lou Foster <lf6f@virginia.edu>
2021-04-01 11:11:18 -0400

    dep updates. remove auth on providers api

--- cmd/main.go
+++ cmd/main.go
@@ -53,7 +53,7 @@ func main() {
 	router.GET("/identify", svc.identifyHandler)
 	api := router.Group("/api")
 	{
-		api.GET("/providers", svc.authMiddleware, svc.providersHandler)
+		api.GET("/providers", svc.providersHandler)
 		api.POST("/search", svc.authMiddleware, svc.search)
 		api.POST("/search/facets", svc.authMiddleware, svc.facets)
 		api.GET("/resource/:id", svc.authMiddleware, svc.getResource)
