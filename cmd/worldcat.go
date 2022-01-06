e1464f3cfabd07dbf0b46f960bc601d546d537ec
Lou Foster <louffoster@gmail.com>
2022-01-06 11:20:04 -0500

    fix format of expire time logging

--- cmd/worldcat.go
+++ cmd/worldcat.go
@@ -312,7 +312,7 @@ func (svc *ServiceContext) getGeneralFormat(id string) ([]byte, error) {
 func (svc *ServiceContext) refreshOCLCAuth() error {
 	log.Printf("INFO: check OCLC auth token")
 	now := time.Now()
-	log.Printf("INFO: token expire %d vs time now %d", svc.OCLC.Expires, now)
+	log.Printf("INFO: token expire %s vs time now %s", svc.OCLC.Expires.Format(time.UnixDate), now.Format(time.UnixDate))
 	if svc.OCLC.Token == "" || svc.OCLC.Expires.After(now) {
 		log.Printf("INFO: requesting new OCLC auth token")
 		err := svc.oclcTokenRequest()
